afcdae1e1c7284189d89f1d90cd772fe957ea717
tmo macbook <trrl.mo@hotmail.com>
2021-08-03 10:04:30 -0400

    Extract 'Healthcare Providers Table' as a React Component.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-function App() {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th colspan="5">Healthcare Providers by NPI</th>
-        </tr>
-        <tr>
-          <th>Name</th>
-          <th>NPI</th>
-          <th>Address</th>
-          <th>Phone Number</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1902809197</td>
-          <td>Dr. JEFFREY DOME MD</td>
-          <td>111 MICHIGAN AVE NW, WASHINGTON, DC, 200102916, United States</td>
-          <td>202-476-2800</td>
-        </tr>
-      </tbody>
-    </table>
-  );
+import React, { Component } from 'react';
+import HealthcareProvidersTable from './components/HealthcareProvidersTable'
+
+class App extends Component {
+  render() {
+    return (
+      <HealthcareProvidersTable/>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/HealthcareProvidersTable.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class HealthcareProvidersTable extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th colspan="5">Healthcare Providers by NPI</th>
+          </tr>
+          <tr>
+            <th>Name</th>
+            <th>NPI</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1902809197</td>
+            <td>Dr. JEFFREY DOME MD</td>
+            <td>111 MICHIGAN AVE NW, WASHINGTON, DC, 200102916, United States</td>
+            <td>202-476-2800</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+}
+
+export default HealthcareProvidersTable
